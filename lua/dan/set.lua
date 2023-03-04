4660f1fe9a8023bf8b65e0bad649b704d83fc89a
SByte <stormybytes@gmail.com>
2023-03-04 20:32:56 +0700

    2 spaced indentation

--- lua/dan/set.lua
+++ lua/dan/set.lua
@@ -3,10 +3,10 @@ vim.g.mapleader = " "
 vim.opt.nu = true
 vim.opt.rnu = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 
 vim.opt.smartindent = true
 
