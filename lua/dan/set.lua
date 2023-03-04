fbbe2da41ff56e682d0bca758264ca6bb55c9acf
SByte <stormybytes@gmail.com>
2023-03-04 09:45:59 +0700

    Initial commit

---
+++ lua/dan/set.lua
@@ -0,0 +1,46 @@
+vim.g.mapleader = " "
+
+vim.opt.nu = true
+vim.opt.rnu = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-d>zz")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
---
+++ set.lua
@@ -0,0 +1,5 @@
+time _ `[all]`
+─────────────────────────────────
+I give you back the time (H:M)
+
+${CURRENT_HOUR}:${CURRENT_MINUTE}
