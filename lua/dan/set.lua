786b62f8ef5222231baf3e8279a7520b25ae532d
SByte <stormybytes@gmail.com>
2023-03-04 20:39:26 +0700

    Fix todo jumps

--- lua/dan/set.lua
+++ lua/dan/set.lua
@@ -48,16 +48,11 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 
 
 -- Todo comments
-vim.keymap.set("n", "]t", function()
-  require("todo-comments").jump_next()
-end, { desc = "Next todo comment" })
 
 vim.keymap.set("n", "[t", function()
-  require("todo-comments").jump_prev()
-end, { desc = "Previous todo comment" })
-
--- You can also specify a list of valid jump keywords
+  require("todo-comments").jump_prev({keywords = { "TODO", "ERROR", "WARNING" }})
+end, { desc = "Previous todo/warning/error todo comment" })
 
 vim.keymap.set("n", "]t", function()
-  require("todo-comments").jump_next({keywords = { "ERROR", "WARNING" }})
-end, { desc = "Next error/warning todo comment" })
+  require("todo-comments").jump_next({keywords = { "TODO", "ERROR", "WARNING" }})
+end, { desc = "Next todo/error/warning todo comment" })
