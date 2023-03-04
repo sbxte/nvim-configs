04ba9fbc31b5fb2ebb14e21e10a51b3a2965c0a2
SByte <stormybytes@gmail.com>
2023-03-04 10:03:01 +0700

    Oops

--- lua/dan/set.lua
+++ lua/dan/set.lua
@@ -35,7 +35,7 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
