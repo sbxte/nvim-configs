fb5b3b8b3f264c810b1c8080d91bddcffe70bd63
SByte <stormybytes@gmail.com>
2023-03-12 11:19:19 +0700

    Properly setup trouble this time

--- lua/sbyte/packer.lua
+++ lua/sbyte/packer.lua
@@ -93,17 +93,7 @@ return require('packer').startup(function(use)
 
 
   -- Trouble (quickfix thing)
-  use {
-    "folke/trouble.nvim",
-    config = function()
-      require("trouble").setup {
-        icons = false,
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
-  }
+  use ("folke/trouble.nvim")
 
 
   -- Lua line status bar
