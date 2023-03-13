484eec42d27cab00e618deab5ce44a03be43f933
SByte <stormybytes@gmail.com>
2023-03-13 07:27:15 +0700

    Increase fuzzy finder ui width and height

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,5 @@
-require('telescope').setup {}
+require('telescope').setup {
+
+}
 
 
--- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
--- lua/sbyte/set.lua
+++ lua/sbyte/set.lua
@@ -112,6 +112,17 @@ map('n', '<leader>/', function()
   tsbuiltin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
     previewer = false,
+    layout_config = {
+      preview_cutoff = 1, -- Preview should always show (unless previewer = false)
+
+      width = function(_, max_columns, _)
+        return math.min(max_columns, 120)
+      end,
+
+      height = function(_, _, max_lines)
+        return math.min(max_lines, 40)
+      end,
+    },
   })
 end, '[/] Fuzzily search in current buffer', 'Telescope')
 map('n', '<leader>sf', tsbuiltin.find_files, '[S]earch [F]iles', 'Telescope')
