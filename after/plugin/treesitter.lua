d1c51c96f74f21dfe9ea71ffbb2a05e23ba1cad3
SByte <stormybytes@gmail.com>
2023-05-21 11:47:31 +0700

    Fix treesitter clang

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,3 +1,4 @@
+require('nvim-treesitter.install').compilers = { "clang" }
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "javascript", "typescript", "rust", "toml", "c", "cpp", "python", "lua", "vim", "help", "query" },
