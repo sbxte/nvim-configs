1c4b7d2e604ce6c32f29026815bbda34a934b9b8
SByte <stormybytes@gmail.com>
2023-07-08 10:29:38 +0700

    Autoread

--- lua/sbyte/set.lua
+++ lua/sbyte/set.lua
@@ -49,6 +49,8 @@ vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decr
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
+vim.o.autoread = true
+
 
 -- Custom remaps
 local category = "Custom"
