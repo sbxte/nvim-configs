23ffb8b3f924cef4dbc55bb79dff70712fd8e90d
SByte <72693226+sbxte@users.noreply.github.com>
2024-06-14 15:32:53 +0700

    Fix rust lsp

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,62 @@
+return {
+	{
+		"mrcjkb/rustaceanvim",
+		opts = {
+			server = {
+				default_settings = {
+					-- rust-analyzer language server configuration
+					["rust-analyzer"] = {
+						cargo = {
+							allFeatures = true,
+							loadOutDirsFromCheck = true,
+							runBuildScripts = true,
+						},
+						-- Add clippy lints for Rust.
+						checkOnSave = {
+							allFeatures = true,
+							command = "clippy",
+							extraArgs = { "--no-deps" },
+						},
+						imports = {
+							granularity = {
+								group = "module",
+							},
+						},
+						inlayHints = { -- This is actually so annoying wtf
+							chainingHints = {
+								enable = false,
+							},
+							closingBraceHints = {
+								enable = false,
+							},
+							parameterHints = {
+								enable = false,
+							},
+							typeHints = {
+								enable = false,
+							},
+						},
+						procMacro = {
+							enable = true,
+							ignored = {
+								["async-trait"] = { "async_trait" },
+								["napi-derive"] = { "napi" },
+								["async-recursion"] = { "async_recursion" },
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			setup = {
+				rust_analyzer = function()
+					return true
+				end,
+			},
+		},
+	},
+}
