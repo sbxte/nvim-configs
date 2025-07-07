c9c6935b854723deea914e56bf8acb631124e6f0
SByte <stormybytes@gmail.com>
2025-07-08 05:33:07 +0700

    Do not use rust-analyzer from nvim-lspconfig
    Use rustaceanvim's instead

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -58,10 +58,8 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		opts = {
-			setup = {
-				rust_analyzer = function()
-					return true
-				end,
+			server = {
+				rust_analyzer = { enabled = false },
 			},
 		},
 	},
