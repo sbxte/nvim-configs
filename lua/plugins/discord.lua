45d09373e75476ace32d0e0197e2c2ac65f0879e
SByte <stormybytes@gmail.com>
2025-07-08 05:50:10 +0700

    Discord RPC

---
+++ lua/plugins/discord.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"vyfor/cord.nvim",
+		lazy = false,
+		opts = {
+			display = {
+				theme = "default",
+				flavor = "dark",
+			},
+			editor = {
+				tooltip = "fingers go brrr",
+			},
+		},
+	},
+}
