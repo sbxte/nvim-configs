a0fc0d80ae497635c48cbf9c51ebb335481d39f6
SByte <stormybytes@gmail.com>
2025-12-28 08:23:00 +0700

    register oil.nvim keymaps to which-key

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,3 +1,10 @@
+-- Returns true when current buffer name starts with "oil:/" aka an oil.nvim buffer
+local in_oil_buf = function()
+	local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
+	local start = bufname.find(bufname, "oil:/")
+	return start == 1
+end
+
 return {
 	{
 		"stevearc/oil.nvim",
@@ -10,20 +17,21 @@ return {
 		---@type oil.SetupOpts
 		opts = {
 			keymaps = {
-				["g?"] = { "actions.show_help", mode = "n" },
-				["<CR>"] = "actions.select",
+				["g?"] = { "actions.show_help", mode = "n", desc = "Show keymaps" },
+				["<CR>"] = { "actions.select" },
 				["<gv>"] = { "actions.select", opts = { vertical = true } },
 				["<gs>"] = { "actions.select", opts = { horizontal = true } },
 				["<gt>"] = { "actions.select", opts = { tab = true } },
-				["gp"] = "actions.preview",
+				["gp"] = { "actions.preview" },
 				["<C-c>"] = { "actions.close", mode = "n" },
-				["r"] = "actions.refresh",
+				["<leader>E"] = { "actions.close", mode = "n" },
+				["r"] = { "actions.refresh" },
 				["-"] = { "actions.parent", mode = "n" },
 				["_"] = { "actions.open_cwd", mode = "n" },
 				["`"] = { "actions.cd", mode = "n" },
 				["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
 				["gs"] = { "actions.change_sort", mode = "n" },
-				["gx"] = "actions.open_external",
+				["gx"] = { "actions.open_external" },
 				["g."] = { "actions.toggle_hidden", mode = "n" },
 				["g\\"] = { "actions.toggle_trash", mode = "n" },
 			},
@@ -42,20 +50,10 @@ return {
 			{
 				"<leader>E",
 				function()
-					local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
-					local start = bufname.find(bufname, "oil:/")
-					if start == 1 then
-						require("oil").close()
-					else
-						---@type oil.OpenOpts
-						local opts = {
-							-- preview = {
-							-- 	split = "belowright",
-							-- 	vertical = true,
-							-- },
-						}
-						require("oil").open(nil, opts, nil)
+					if in_oil_buf() then
+						return
 					end
+					require("oil").open(nil, {}, nil)
 				end,
 				desc = "Explorer Oil",
 				expr = false,
@@ -63,6 +61,30 @@ return {
 			{ "<leader>fe", "<leader>e", desc = "Explorer Neotree", expr = true },
 			{ "<leader>fE", "<leader>E", desc = "Explorer Oil", expr = true },
 		},
+		config = function(_, opts)
+			local wk = require("which-key")
+			for k, v in pairs(opts.keymaps) do
+				local mapping = {
+					k,
+					group = "Oil",
+					desc = v.desc,
+					cond = in_oil_buf,
+					mode = v.mode or "n",
+				}
+
+				if type(v[1]) == "string" then
+					if string.find(v[1], "actions.") == 1 then
+						local action = string.sub(v[1], 9)
+						local actions = require("oil.actions")
+						mapping.desc = mapping.desc or actions[action].desc
+					end
+				end
+
+				wk.add(mapping)
+			end
+
+			require("oil").setup(opts)
+		end,
 	},
 	{
 		"nvim-neo-tree/neo-tree.nvim",
