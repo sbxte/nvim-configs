987b2b78ded2c0ee82863f7ddc89433d251adf06
SByte <stormybytes@gmail.com>
2026-03-14 08:13:55 +0700

    fzflua find in current buffer and all buffers

---
+++ lua/plugins/fzf.lua
@@ -0,0 +1,9 @@
+return {
+	{
+		"ibhagwan/fzf-lua",
+		keys = {
+			{ "<leader>sb", "<cmd>FzfLua blines<cr>", desc = "Current Buffer Lines" },
+			{ "<leader>sB", "<cmd>FzfLua lines<cr>", desc = "All Buffer Lines" },
+		},
+	},
+}
